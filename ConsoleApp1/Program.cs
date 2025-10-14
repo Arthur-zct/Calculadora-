c185eb2d3bdfee783d057068059b302a68eb6a80
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-10-14 20:05:12 -0300

    Calculadora aperfeiçoada

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,52 +4,72 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Calculadora Simples \n");
-        Console.WriteLine("Digite o primeiro número: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
+        bool continuar = true;
 
-        Console.WriteLine("Digite o segundo número: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        void CalculadoraSimples ()
+        {
+            Console.WriteLine("\nIniciar calculo \n");
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
 
-        //Selecionar o tipo da operação.
-        Console.WriteLine("Selecione a operação:");
-        Console.WriteLine("1 - Soma (+)");
-        Console.WriteLine("2 - Subtração (-)");
-        Console.WriteLine("3 - Multiplicação (*)");
-        Console.WriteLine("4 - Divisão (/)");
-        int operacao = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
-        //Condições
-        double result = 0;
-        if (operacao == 1)
-        {
-            result = num1 + num2;
-        }
-        else if (operacao == 2)
-        {
-            result = num1 - num2;
-        }
-        else if (operacao == 3)
-        {
-            result = num1 * num2;
-        }
-        else if (operacao == 4)
-        {
-            if (num2 != 0)
+            //Selecionar o tipo da operação.
+            Console.WriteLine("Selecione a operação:");
+            Console.WriteLine("1 - Soma (+)");
+            Console.WriteLine("2 - Subtração (-)");
+            Console.WriteLine("3 - Multiplicação (*)");
+            Console.WriteLine("4 - Divisão (/)");
+            int operacao = Convert.ToInt32(Console.ReadLine());
+
+            //Condições
+            double result = 0;
+            if (operacao == 1)
             {
-                result = num1 / num2;
+                result = num1 + num2;
+            }
+            else if (operacao == 2)
+            {
+                result = num1 - num2;
+            }
+            else if (operacao == 3)
+            {
+                result = num1 * num2;
+            }
+            else if (operacao == 4)
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                    return;
+                }
             }
             else
             {
-                Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                Console.WriteLine("Operação inválida.");
                 return;
             }
+            Console.WriteLine($"O resultado é: {result} \n");
+
+            Console.WriteLine("Deseja realizar outro cálculo? (s/n) \n");
+            string response = Console.ReadLine().ToLower();
+            if (response != "s")
+            {
+                continuar = false;
+            }
         }
-        else
-        {
-            Console.WriteLine("Operação inválida.");
-            return;
+
+        
+        Console.WriteLine("Calculadora Simples \n");
+        while (continuar) { 
+            CalculadoraSimples();
         }
-        Console.WriteLine($"O resultado é: {result}");
+
+        
     }
 }
